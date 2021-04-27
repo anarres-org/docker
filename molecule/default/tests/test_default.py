43792410773acaf25129544aa4fe58829a47017e
m0wer <m0wer@autistici.org>
2021-04-27 22:02:51 +0200

    fix: update pip package name

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,30 +3,36 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize("package", [
-    ("docker-ce"),
-    ("python-pip"),
-])
+@pytest.mark.parametrize(
+    "package",
+    [
+        ("docker-ce"),
+        ("python3-pip"),
+    ],
+)
 def test_required_packages_exist(host, package):
     pkg = host.package(package)
     assert pkg.is_installed
 
 
-@pytest.mark.parametrize("pip_package", [
-    ("docker"),
-])
+@pytest.mark.parametrize(
+    "pip_package",
+    [
+        ("docker"),
+    ],
+)
 def test_required_pip_packages_exist(host, pip_package):
     pip_packages = host.pip_package.get_packages()
     assert pip_package in pip_packages
 
 
 def test_gpg_through_key_apt_repository_file(host):
-    f = host.file('/etc/apt/sources.list.d/download_docker_com_linux_debian.li'
-                  'st')
+    f = host.file("/etc/apt/sources.list.d/download_docker_com_linux_debian.li" "st")
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-    assert f.contains('https://download.docker.com/linux/debian')
+    assert f.user == "root"
+    assert f.group == "root"
+    assert f.contains("https://download.docker.com/linux/debian")
